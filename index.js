d636deb3b7ee6d4f87674ba3d27ca4277edad2fc
Stuart Lynn <stuart.lynn@gmail.com>
2015-07-29 16:36:13 -0400

    whitespace

--- index.js
+++ index.js
@@ -54,6 +54,5 @@ var port = (process.env.PORT || 5000)
 var server = app.listen(port, function () {
   var host = server.address().address;
   var port = server.address().port;
-
   console.log('Example app listening at http://%s:%s', host, port);
 });
